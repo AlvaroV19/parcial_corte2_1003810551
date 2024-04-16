1381de697cb16c26ba9aef65e3dbb804c94a3815
AlvaroV19 <afavila-2022a@corhuila.edu.co>
2024-04-16 09:59:40 -0500

    cambios

---
+++ loggin/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RegisterComponent } from './components/users/register/register.component';
+
+@NgModule({
+  declarations: [AppComponent, RegisterComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
